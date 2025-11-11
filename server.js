6b65bca8848a733aaf0e531ae95d6e2cb8a1c162
Pedro Teixeira <pedro.progteixeira@gmail.com>
2025-11-11 10:22:08 +0000

    refactor(server): move route logic into controllers with dependency injection

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+const register = require("./controllers/register");
+const signIn = require("./controllers/signIn");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+const clarifai = require("./controllers/clarifai");
+
+
+
 require("dotenv").config();
 
 const db = knex({
@@ -25,119 +33,18 @@ app.get("/", (req, res) => {
   // res.json({ status: 'success' });
 });
 
-app.post("/signin", (req, res) => {
-  const { email, password } = req.body;
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
-      if(isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('Unable to get user'))
-      } else {
-        res.status(400).json('Wrong credentials')
-      } 
-    })
-    .catch(err => res.status(400).json('Wrong Credentials'))
-});
+app.post("/signin", (req, res) => {signIn.handleSignIn(req, res, db, bcrypt)});
 
-app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("Unable to register!"));
-});
+app.post("/register", (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
-app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-  db.select("*")
-    .from("users")
-    .where({
-      id: id,
-    })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("Not Found");
-      }
-    })
-    .catch((err) => res.status(400).json("Error getting user"));
-});
 
-app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => {
-      res.json(entries[0].entries);
-    })
-    .catch((err) => res.status(400).json("Unable to get entries"));
-});
+
+app.get("/profile/:id", (req, res) => {profile.handleProfileGet(req, res, db)});
+
+app.put("/image", (req, res) => {image.handleImage(req, res, db)});
 
 // New api route to communicate with clarifai
-app.post("/api/clarifai/face-detect", async (req, res) => {
-  try {
-    const {
-      imageUrl,
-      modelId = "face-detection",
-      userId = "clarifai",
-      appId = "main",
-    } = req.body;
-    if (!imageUrl) return res.status(400).json({ error: "mandatory imageUrl" });
-
-    const body = {
-      user_app_id: { user_id: userId, app_id: appId },
-      inputs: [{ data: { image: { url: imageUrl } } }],
-    };
-
-    const r = await fetch(
-      `https://api.clarifai.com/v2/models/${modelId}/outputs`,
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          Authorization: `Key ${process.env.CLARIFAI_PAT}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      }
-    );
-
-    const data = await r.json();
-    return res.status(r.status).json(data);
-  } catch (e) {
-    return res.status(500).json({ error: "proxy_failed", details: e.message });
-  }
-});
+app.post("/api/clarifai/face-detect", (req, res)  => {clarifai.handleClrarifai(req, res)});
 
 app.listen(3000, () => {
   console.log("App is running on port 3000");
