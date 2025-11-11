62595fb21f87bf8d1b4c89dd35866854de7e727d
Pedro Teixeira <pedro.progteixeira@gmail.com>
2025-11-11 11:51:41 +0000

    fix(api): add input validation to register and signin endpoints

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,6 +1,11 @@
 
 const handleSignIn = (req, res, db, bcrypt) => {
   const { email, password } = req.body;
+
+  if(!email || !password) {
+    return res.status(400).json('Incorrect form submission');
+  }
+
   db.select("email", "hash")
     .from("login")
     .where("email", "=", email)
