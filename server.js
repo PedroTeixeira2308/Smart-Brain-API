33a51e71df913c8d14c55375b55906e5c2c629b2
Pedro Teixeira <pedro.progteixeira@gmail.com>
2025-11-11 16:04:33 +0000

    chore(env): Move server port to environment variable

--- server.js
+++ server.js
@@ -46,6 +46,8 @@ app.put("/image", (req, res) => {image.handleImage(req, res, db)});
 // New api route to communicate with clarifai
 app.post("/api/clarifai/face-detect", (req, res)  => {clarifai.handleClrarifai(req, res)});
 
-app.listen(3000, () => {
-  console.log("App is running on port 3000");
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`App is running on port ${process.env.PORT}`);
 });
